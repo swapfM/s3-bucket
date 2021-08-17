7818329407fa1d4fe24e134c49938908c897db86
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-17 23:27:55 +0530

    Update settings.py

--- drf_aws/settings.py
+++ drf_aws/settings.py
@@ -134,11 +134,3 @@ def delete(self):
     self.filefield.delete(save=False)
     super().delete()
 
-AWS_ACCESS_KEY_ID = "AKIAVEDQS72LOQQGXIUX"
-AWS_SECRET_ACCESS_KEY = "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"
-AWS_STORAGE_BUCKET_NAME = "wikivid"
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_SIGNATURE_VERSION = "s3v4"
-AWS_S3_REGION_NAME = "ap-south-1"
